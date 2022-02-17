f719a1346fa8bd43c489b98e2d205fb5ad86912a
pjerryhu <pengxiang.hu@cmu.edu>
2022-02-16 20:04:36 -0800

    updating the twitter handle

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import CandyMachine from './CandyMachine';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'husjerry1';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
